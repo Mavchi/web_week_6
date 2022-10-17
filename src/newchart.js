de7eab33d9ec2a3d52151f98405c717e048b7677
Aleksander Khlebnikov <aleksi.khlebnikov@gmail.com>
2022-10-17 22:53:12 +0000

    Update newchart.js

--- src/newchart.js
+++ src/newchart.js
@@ -2,10 +2,10 @@ import "./styles.css";
 
 import { Chart } from "frappe-charts/dist/frappe-charts.min.esm";
 
-let jsonQuery = {};
-let birthData = [];
-let deathData = [];
-let municipalityName = "";
+let jsonQuery = undefined;
+let birthData = undefined;
+let deathData = undefined;
+let municipalityName = undefined;
 
 const loadData = async () => {
   const url =
